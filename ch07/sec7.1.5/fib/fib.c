bc6729eb9fd297fa55a55480ea794c49d3857279
Feyerabend <setlonnert@gmail.com>
2026-02-20 09:40:37 +0100

    Create fib.c

---
+++ ch07/sec7.1.5/fib/fib.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int fibonacci(int n) {
+    if (n <= 1) 
+        return n;
+
+    return fibonacci(n - 1) + fibonacci(n - 2);
+}
+
+
+int main() {
+    int n = 10; // compute Fibonacci number
+    printf("Fibonacci(%d) = %d\n", n, fibonacci(n));
+    return 0;
+}
